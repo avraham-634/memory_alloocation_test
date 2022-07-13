975db7b1ca927f6ea1ec0e48405ca6233aa92251
nat526 <nathangigi526@gmail.com>
2022-07-13 21:41:04 +0300

    rectification

--- IntAllocatorBase.cpp
+++ IntAllocatorBase.cpp
@@ -1,46 +1,20 @@
+#include <iostream>
+#include <array>
 class IntAllocatorBase {
 	public:
-		virtual int* allocate(size_t size) = 0;
+		int* allocate(int size){
+		    return new int[size];
+		    //or return (int*)malloc(size*sizeof(int));
+		}
 		
-		virtual void deallocate(int* ptr, size_t size) = 0;
+		void deallocate(int* ptr, int size){
+		    for (size_t i = 0; i<size; i++){
+		        delete (ptr+i);
+		    }
+		    // or delete [] ptr;
+		    ptr = NULL;
+		}
 	private:
-		std::array<int, 100> m_buffer;
+		std::array<int,100> m_buffer;
 
-        void insert(int val){
-            // allocate new data memory with size +1
-            ptr = allocate(m_buffer.size+1);
-            //copy the data of the old array to the new array
-            for (int i = 0; i < m_buffer.size; i++):
-                ptr[i] = m_buffer[i];
-            //insert the new elem
-            ptr[m_buffer.size] = val;
-            //deallocate the old array
-            deallocate(m_buffer, m_buffer.size);
-            //assign the new array data to the m_buffer
-            m_buffer = ptr;
-        }
-
-        void remove(int val){
-            // allocate new data memory with size -1
-            ptr = allocate(m_buffer.size-1);
-            for (int i,j = 0; i < m_buffer.size;i++):{
-                //copy the data of the old array to the new array
-                //only the elem to remove
-                if (m_buffer[i] != val):
-                    //while the curent elem different of val,
-                    //then copy to the new arr and up 'j' index
-                    ptr[j] = m_buffer[i];
-                    j+=1
-                //else don't copy and don't up j; but up i
-            }
-            //deallocate the old array
-            deallocate(m_buffer, m_buffer.size);
-            //assign the new array data to the m_buffer
-            m_buffer = ptr;
-        }
-
-        void empty(){
-            deallocate(m_buffer, m_buffer.size);
-            m_buffer = allocate(0);
-        }
 	};
